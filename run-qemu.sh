1239f52470bbc4b34b202cb6be86065ccf5765ff
cys <chaoys155@gmail.com>
2016-04-23 20:08:31 +0800

    remove const numbers

--- run-qemu.sh
+++ run-qemu.sh
@@ -3,6 +3,8 @@
 qemu="/home/cys/bin/qemu-2.5/bin/qemu-system-x86_64"
 USER_SOCK="/tmp/user.uds"
 MPATH="/home/cys/huge"
+NPAGE=300
+MEMSZ=256
 
 cat /proc/mounts | grep huge >/dev/null
 if [ $? -ne 0 ]; then
@@ -13,17 +15,17 @@ fi
 # assuming 2MB page
 num_huge_page=`cat /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages`
 if [ ${num_huge_page} -eq 0 ]; then
-	sudo bash -c 'echo 300 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages'
+	sudo bash -c "echo ${NPAGE} > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
 fi
 
 num_huge_page=`cat /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages`
-#if [ ${num_huge_page} -ne 300 ]; then
+#if [ ${num_huge_page} -ne ${NPAGE} ]; then
 #	echo "cannot run: only $num_huge_page hugepages reserved"
 #	exit 1
 #fi
 
-sudo ${qemu} -enable-kvm -m 256 \
-		-object memory-backend-file,id=hugemem,size=256M,mem-path=${MPATH},share=on \
+sudo ${qemu} -enable-kvm -m ${MEMSZ} \
+		-object memory-backend-file,id=hugemem,size=${MEMSZ}M,mem-path=${MPATH},share=on \
 		-numa node,memdev=hugemem \
 		-chardev socket,id=uds0,path=${USER_SOCK} -netdev vhost-user,id=user0,chardev=uds0 -device virtio-net-pci,id=net0,netdev=user0 \
 		-drive file=~/data/vm-images/qemu/tinycore.raw,format=raw \
